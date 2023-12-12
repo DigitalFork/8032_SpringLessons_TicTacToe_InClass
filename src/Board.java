edc057d73e9896b724fa48e4f4a09f3e29b5e96c
DigitalFork <113265535+DigitalFork@users.noreply.github.com>
2023-12-11 22:06:43 -0800

    Fully functional tic-tac-toe with many options

--- src/Board.java
+++ src/Board.java
@@ -3,15 +3,16 @@ public class Board {
     private final char DEFAULT_TOKEN = '_';
 	private int boardWidth;
 	private int boardHeight;
+	private int boardBoxWidth;
 	private int boardWinLength;
-	private char[] players =  {DEFAULT_TOKEN, 'X', 'O'};
+	private char[] players =  {DEFAULT_TOKEN, 'X', 'O', 'L'};
 
 	private int turn = 1;
 
     // State of the tic tac toe board
     // note: everything to the right of the '=' could be done in constructor instead
     //       This is a style choice.
-    private char[][] board = new char[3][3];
+    private char[][] board;
 
     /**
 	 * Initialize board with width, height, and run length to win of 3
@@ -42,15 +43,18 @@ public class Board {
     {
 		initBoard(width, height, winLength);
 	}
+	
 	void initBoard (int width, int height, int winLength)
 	{
 		boardWidth = width;
 		boardHeight = height;
 		boardWinLength = winLength;
+		boardBoxWidth = String.valueOf(width * height).length();
+		board = new char[height][width];
 
-		for(int row = 0; row < width; ++row)
+		for(int row = 0; row < height; ++row)
 		{
-			for(int col = 0; col < height; ++col)
+			for(int col = 0; col < width; ++col)
 			{
 				board[row][col] = DEFAULT_TOKEN;
 			}
@@ -84,55 +88,92 @@ public class Board {
 
     public boolean isGameOver()
     { 
-		// // If getWinner does not return null,
-		// // then someone won or tied, and the game is over, so return true
-		// return '\0' != getWinner();
-		return false;
+		// If getWinner does not return null,
+		// then someone won or tied, and the game is over, so return true
+		return getWinner() != '\0';
 	}
 	
-	public char getWinner() {
+	public char getWinner()
+	{
+		char winToken = '\0';
+		
 		IntToBoardChar rowRun = (i,j) -> board[i][j];
-		IntToBoardChar colRun = (i,j) -> board[j][i];
-
         for (int row = 0; row < boardHeight; ++row)
 		{
-
 			char runToken = testRun(boardWidth, row, rowRun);
+			if (runToken != DEFAULT_TOKEN)
+			{
+				winToken = runToken;
+			}
+			
 		}
+		
+		IntToBoardChar colRun = (i,j) -> board[j][i];
 		for (int col = 0; col < boardWidth; ++col)
 		{
 			char runToken = testRun(boardHeight, col, colRun);
-		}
-		//Old code
-		/* for (int i = 0; i < boardHeight; ++i)
-		{
-			int runCount = 0;
-			char runToken = DEFAULT_TOKEN;
-			for (int j = 0; j < boardWidth; ++j)
+			if (runToken != DEFAULT_TOKEN)
 			{
-				runCount = (board[i][j] == runToken) ? (runCount + 1) : 1;
-				runToken = board[i][j];
-				if (runCount >= boardWinLength & runToken != DEFAULT_TOKEN)
-				{
-					return runToken;
-				}
+				winToken = runToken;
 			}
 		}
-		for (int i = 0; i < boardWidth; ++i)
+
+		// Check diagonals starting from the left side and going either up or down to the right
+		IntToBoardChar ascDiagonalRun = (i,j) -> board[i + j][j];
+		IntToBoardChar descDiagonalRun = (i,j) -> board[boardHeight - i - j - 1][j];
+		for (int startRow = 0; startRow <= boardHeight - boardWinLength; ++startRow)
 		{
-			int runCount = 0;
-			char runToken = DEFAULT_TOKEN;
-			for (int j = 0; j < boardHeight; ++j)
+			char runAscToken = testRun(Math.min(boardWidth, boardHeight - startRow), startRow, ascDiagonalRun);
+			if (runAscToken != DEFAULT_TOKEN & winToken == '\0')
 			{
-				runCount = (board[j][i] == runToken) ? (runCount + 1) : 1;
-				runToken = board[j][i];
-				if (runCount >= boardWinLength & runToken != DEFAULT_TOKEN)
-				{
-					return runToken;
-				}
+				winToken = runAscToken;
 			}
-		} */
-		return DEFAULT_TOKEN;
+			char runDescToken = testRun(Math.min(boardWidth, boardHeight - startRow), startRow, descDiagonalRun);
+			if (runDescToken != DEFAULT_TOKEN & winToken == '\0')
+			{
+				winToken = runDescToken;
+			}
+		}
+		// Check diagonals starting from the bottom or top and going either up or down to the right
+		IntToBoardChar ascDiagonalRun2 = (i,j) -> board[j][i + j];
+		IntToBoardChar descDiagonalRun2 = (i,j) -> board[boardHeight - j - 1][i + j];
+		for (int startCol = 1; startCol < boardWidth; ++startCol)
+		{
+			char runAscToken = testRun(Math.min(boardWidth - startCol, boardHeight), startCol, ascDiagonalRun2);
+			if (runAscToken != DEFAULT_TOKEN & winToken == '\0')
+			{
+				winToken = runAscToken;
+			}
+			char runDescToken  = testRun(Math.min(boardWidth - startCol, boardHeight), startCol, descDiagonalRun2);
+			if(runDescToken != DEFAULT_TOKEN & winToken == '\0')
+			{
+				winToken = runDescToken;
+			}
+		}
+
+		if (winToken != '\0')
+		{
+			return winToken;
+		}
+		boolean isTie = true;
+		for (int row = 0; row < boardHeight; ++row)
+		{
+			for (int col = 0; col < boardWidth; ++col)
+			{
+				// If isTie still hasn't been set to false and the current board square isn't empty,
+				// then isTie will stay true.
+				// Otherwise, isTie will be set to false and can't be set back to true
+				isTie = isTie & board[row][col] != DEFAULT_TOKEN;
+			}
+		}
+		if (isTie)
+		{
+			return DEFAULT_TOKEN; 
+		}
+		else
+		{
+			return '\0';
+		}
 
 	}
 
@@ -140,77 +181,126 @@ public class Board {
     {
         StringBuilder builder = new StringBuilder();
 
-        for(int row = 0; row < boardHeight; ++row)
+		// Row is seperate from iterator because there are more display rows than board rows
+		int row = 0;
+        for(int i = 0; i < 2 * boardHeight - 1; ++i)
         {
-            for(int col = 0; col < boardWidth; ++col)
-            {
-				char boardChar = board[row][col];
-				
-				builder.append(' ');
-				
-				if (boardChar == DEFAULT_TOKEN)
-				{
-					builder.append(row * boardWidth + col + 1);
+			for(int col = 0; col < boardWidth; ++col)
+			{
+				// even rows are game pieces, odd rows are seperators
+				if (i % 2 == 0) {
+
+					char boardChar = board[row][col];
+					
+					builder.append(' ');
+					
+					if (boardChar == DEFAULT_TOKEN)
+					{
+						int squareVal = row * boardWidth + col + 1;
+						for (int spaces = 0; spaces < boardBoxWidth - String.valueOf(squareVal).length(); spaces++)
+						{
+							builder.append(' ');
+						}
+						builder.append(squareVal);
+					}
+					else
+					{
+						for (int spaces = 0; spaces < boardBoxWidth - 1; spaces++)
+						{
+							builder.append(' ');
+						}
+						builder.append(boardChar);
+					}
+					builder.append(' ');
+					
+					// Add seperation if not at end of line
+					
+					/*
+					*  1 | 2 | 3
+					* ———|———|———
+					*  X | O | 6
+					* ———|———|———
+					*  O | 7 | 8
+					*/
 				}
 				else
 				{
-					builder.append(boardChar);
+					for (int spaces = 0; spaces < boardBoxWidth + 2; spaces++)
+					{
+						builder.append("-");
+					}
 				}
-				builder.append(' ');
-
-				// Add seperation if 
+				
 				if (col < boardWidth - 1)
 				{
 					builder.append('|');
 				}
-
-				/*
-				 *  1 | 2 | 3
-				 * ———|———|———
-				 *  X | O | 6
-				 * ———|———|———
-				 *  O | 7 | 8
-				 */
             }
             builder.append('\n');
-			if (row < boardHeight - 1) {
-				builder.append("---|---|---\n");
+
+			if (i % 2 == 0)
+			{
+				row++;
 			}
         }
-
+		
         return builder.toString();
     }
 	
-	public int getTurn() {
+	public int getTurn()
+	{
 		return turn;
 	}
-	public char getTurnChar() {
+	public char getTurnChar()
+	{
 		return players[turn];
 	}
-	public void setTurn(int turn) {
+	public void setTurn(int turn)
+	{
 		this.turn = turn;
 	}
-	public int changeTurn() {
+	public int changeTurn()
+	{
 		// If turn reaches the end of the player list, reset to zero, then increment
 		turn = turn % (players.length - 1) + 1;
 		return turn;
 	}
-
+	public char getDefaultToken() 
+	{
+		return DEFAULT_TOKEN;
+	}
+	
 	private char testRun(int iterations, int fixedIndex, IntToBoardChar getChar)
 	{
-		char runToken = DEFAULT_TOKEN;
+		char runToken = '\0';
 		int runCount = 0;
 		for (int j = 0; j < iterations; ++j)
 		{
-			runCount = (getChar.op(fixedIndex, j) == DEFAULT_TOKEN) ? runCount + 1 : 1;
+			runCount = (getChar.op(fixedIndex, j) == runToken) ? runCount + 1 : 1;
 			runToken = getChar.op(fixedIndex, j);
 			if (runCount >= boardWinLength)
 			{
 				return runToken;
 			}
 		}
-		return '_';
+		// if (runCount > 1 & runToken != DEFAULT_TOKEN & runToken != '\0')
+		// {
+		// 	System.out.println("Run count: " + runCount);
+		// }
+		if (runCount >= boardWinLength)
+		{
+			return runToken;
+		}
+		else
+		{
+			// If no run occured, return default token
+			return DEFAULT_TOKEN;
+		}
 	}
+	/**
+	 * An interface holding a function that can be passed as a parameter
+	 * This is used  for the testRun() function
+	 */
 	private interface IntToBoardChar {
 		public char op(int i, int j);
 	}
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -10,16 +10,16 @@ public class TicTacToe
     ✅ there can be two players
     ✅ players take turns (i.e. player 0 goes, then player 1 goes, etc)
     ✅ players input their turns in the terminal while the main loop is running
-    - there can be a winner
-    - there can be a tie
-    - all fields in the Board class are private
+    ✅ there can be a winner
+    ✅ there can be a tie
+    ✅ all fields in the Board class are private
     */
 
     public static void main(String[] args) throws Exception
     {
         System.out.println("Hello, World!");
 
-        Board board = new Board(3, 3);
+        Board board = new Board(5, 5, 4);
 
         // A BufferedReader allows you to take in input strings from the terminal
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -29,7 +29,6 @@ public class TicTacToe
         {
             System.out.print("\033[H\033[2J");  
             System.out.flush();
-            // An example of how to use BufferedReader:
             System.out.println(board.displayBoard());
 			System.out.println("Player " + board.getTurnChar() + ", make your move");
 			
@@ -50,7 +49,17 @@ public class TicTacToe
 			}
 
         }
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+		System.out.println(board.displayBoard());
 
-        // TODO print out who won! (or if there was a tie)
+		if (board.getWinner() != board.getDefaultToken() & board.getWinner() != '\0')
+		{
+			System.out.println("Congratulations player " + board.getWinner() + ", you won!");
+		}
+		else
+		{
+			System.out.println("The game was a tie!");
+		}
     }
 }
